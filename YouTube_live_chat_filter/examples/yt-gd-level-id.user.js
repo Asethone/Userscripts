ffc7419635161638178118de7e06b4f3e16adef4
Mikhail Lobotskiy <Mikhail.Lobotskiy@onlyoffice.com>
2023-10-14 19:07:59 +0400

    Gd script update

--- YouTube_live_chat_filter/examples/yt-gd-level-id.user.js
+++ YouTube_live_chat_filter/examples/yt-gd-level-id.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube live chat filter GD level IDs
 // @namespace    https://github.com/Asethone/Userscripts/tree/main/YouTube_live_chat_filter/
-// @version      0.1.1
+// @version      0.1.3
 // @description  Redirect live chat messages with only GD level identificators to special popup window
 // @author       Asethone
 // @match        https://www.youtube.com/live_chat*
@@ -14,7 +14,7 @@
 (function () {
     'use strict'
 
-    console.log('SCRIPT IS WORKING...');
+    console.log('SCRIPT IS WORKING.....');
     // Data
     let isActive = false;       // is message tracking active
     let viewWindow = null;      // view popup window
@@ -36,101 +36,103 @@
         const windowInitHeight = Math.floor(window.screen.height * 0.8);
         // Open popup
         viewWindow = window.open('', 'View', `popup=yes,width=${windowInitWidth},height=${windowInitHeight}`);
-        viewWindow.document.title = 'View';
-        const style = viewWindow.document.createElement('style');
-        style.textContent = `
-            body {
-                background-color: #0f0f0f;
-                color: #ffffff;
-            }
+        if (!viewWindow.document.title) {
+            viewWindow.document.title = 'View';
+            const style = viewWindow.document.createElement('style');
+            style.textContent = `
+                body {
+                    background-color: #0f0f0f;
+                    color: #ffffff;
+                }
 
-            p {
-                margin: 0;
-                overflow-wrap: break-word;
-            }
+                p {
+                    margin: 0;
+                    overflow-wrap: break-word;
+                }
 
-            #list {
-                width: 100%;
-                min-width: 400px;
-                max-width: 800px;
-                margin: 0 auto;
-            }
+                #list {
+                    width: 100%;
+                    min-width: 400px;
+                    max-width: 800px;
+                    margin: 0 auto;
+                }
 
-            #list>div {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                font-family: Roboto, Arial, sans-serif;
-                font-size: 20px;
-                margin-bottom: 8px;
-                padding: 5px;
-                background-color: #ffffff1a;
-                border-radius: 5px;
-            }
+                #list>div {
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                    font-family: Roboto, Arial, sans-serif;
+                    font-size: 20px;
+                    margin-bottom: 8px;
+                    padding: 5px;
+                    background-color: #ffffff1a;
+                    border-radius: 5px;
+                }
 
-            #list img {
-                width: 30px;
-                height: 30px;
-                border-radius: 30px;
-                margin-right: 10px;
-            }
+                #list img {
+                    width: 30px;
+                    height: 30px;
+                    border-radius: 30px;
+                    margin-right: 10px;
+                }
 
-            #list .author {
-                min-width: 120px;
-                width: 20%;
-                margin-right: 10px;
-                color: #9a9a9a;
-            }
+                #list .author {
+                    min-width: 120px;
+                    width: 20%;
+                    margin-right: 10px;
+                    color: #9a9a9a;
+                }
 
-            #list .message {
-                width: 85%;
-                color: #e8e8e8;
-            }
+                #list .message {
+                    width: 85%;
+                    color: #e8e8e8;
+                }
 
-            #list button {
-                display: block;
-                width: 30px;
-                height: 30px;
-                border: none;
-                background-color: #ffffff25;
-                border-radius: 5px;
-                color: #e8e8e8;
-            }
+                #list button {
+                    display: block;
+                    width: 30px;
+                    height: 30px;
+                    border: none;
+                    background-color: #ffffff25;
+                    border-radius: 5px;
+                    color: #e8e8e8;
+                }
 
-            #list button:hover {
-                background-color: #ff0000;
-            }
-        `;
-        viewWindow.document.head.appendChild(style);
-        viewWindow.document.body.innerHTML = '<div id="list"></div>';
-        const script = viewWindow.document.createElement('script');
-        script.textContent = `
-            function addMessage(imgSrc, author, message) {
-                const list = document.getElementById('list');
-                const div = document.createElement('div');
-                const img = document.createElement('img');
-                img.setAttribute('src', imgSrc);
-                const pAuthor = document.createElement('p');
-                pAuthor.textContent = author;
-                pAuthor.className = 'author';
-                const pMessage = document.createElement('p');
-                pMessage.textContent = message;
-                pMessage.className = 'message';
-                const btn = document.createElement('button');
-                btn.textContent = '✕';
-                btn.onclick = () => { div.remove() };
-                div.appendChild(img);
-                div.appendChild(pAuthor);
-                div.appendChild(pMessage);
-                div.appendChild(btn);
-                list.appendChild(div);
-            }
+                #list button:hover {
+                    background-color: #ff0000;
+                }
+            `;
+            viewWindow.document.head.appendChild(style);
+            viewWindow.document.body.innerHTML = '<div id="list"></div>';
+            const script = viewWindow.document.createElement('script');
+            script.textContent = `
+                function addMessage(imgSrc, author, message) {
+                    const list = document.getElementById('list');
+                    const div = document.createElement('div');
+                    const img = document.createElement('img');
+                    img.setAttribute('src', imgSrc);
+                    const pAuthor = document.createElement('p');
+                    pAuthor.textContent = author;
+                    pAuthor.className = 'author';
+                    const pMessage = document.createElement('p');
+                    pMessage.textContent = message;
+                    pMessage.className = 'message';
+                    const btn = document.createElement('button');
+                    btn.textContent = '✕';
+                    btn.onclick = () => { div.remove() };
+                    div.appendChild(img);
+                    div.appendChild(pAuthor);
+                    div.appendChild(pMessage);
+                    div.appendChild(btn);
+                    list.appendChild(div);
+                }
 
-            function setOnCloseCallback(fn) {
-                window.onbeforeunload = () => fn();
-            }
-        `;
-        viewWindow.document.body.appendChild(script);
+                function setOnCloseCallback(fn) {
+                    window.onbeforeunload = () => fn();
+                }
+            `;
+            viewWindow.document.body.appendChild(script);
+        }
         // Set popup's onclose callback
         viewWindow.setOnCloseCallback(() => {
             updateStatus(false);
@@ -155,40 +157,43 @@
     // Scrap chat messages
     const msgList = document.querySelector('#chat #items');
     const onAppend = function (appendedNode) {
-        if (viewWindow) {
-            // timeout just in case images src are not yet loaded correctly
-            setTimeout(() => {
-                const message = (() => {
-                    const elMsg = appendedNode.querySelector('#message');
-                    let strMsg = '';
-                    for (const node of elMsg.childNodes) {
-                        if (node.nodeType === Node.TEXT_NODE) {
-                            strMsg += node.textContent;
-                        } else if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'IMG') {
-                            let emoji = node.getAttribute('alt');
-                            if (emoji)
-                                strMsg += emoji;
-                        }
+        if (viewWindow === null)
+            return;
+        // if element is not a chat message (may be some youtube alerts, notifications, etc.) do not handle it
+        if (appendedNode.tagName != 'YT-LIVE-CHAT-TEXT-MESSAGE-RENDERER')
+            return;
+        // timeout just in case images src are not yet loaded correctly
+        setTimeout(() => {
+            const message = (() => {
+                const elMsg = appendedNode.querySelector('#message');
+                let strMsg = '';
+                for (const node of elMsg.childNodes) {
+                    if (node.nodeType === Node.TEXT_NODE) {
+                        strMsg += node.textContent;
+                    } else if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'IMG') {
+                        let emoji = node.getAttribute('alt');
+                        if (emoji)
+                            strMsg += emoji;
                     }
-                    return strMsg;
-                })();
-                // filter message
-                const messageContents = filterMessage(message);
-                if (!messageContents)
-                    return;
-                // get author image and name
-                const imgSrc = appendedNode.querySelector('#img').getAttribute('src');
-                const author = appendedNode.querySelector('#author-name').textContent;
-                // send message to popup window
-                if (Array.isArray(messageContents)) {
-                    for (const content of messageContents) {
-                        viewWindow.addMessage(imgSrc, author, content);
-                    }
-                } else {
-                    viewWindow.addMessage(imgSrc, author, messageContents);
                 }
-            }, 100);
-        }
+                return strMsg;
+            })();
+            // filter message
+            const messageContents = filterMessage(message);
+            if (!messageContents)
+                return;
+            // get author image and name
+            const imgSrc = appendedNode.querySelector('#img').getAttribute('src');
+            const author = appendedNode.querySelector('#author-name').textContent;
+            // send message to popup window
+            if (Array.isArray(messageContents)) {
+                for (const content of messageContents) {
+                    viewWindow.addMessage(imgSrc, author, content);
+                }
+            } else {
+                viewWindow.addMessage(imgSrc, author, messageContents);
+            }
+        }, 100);
     };
     // Mutation callback
     const callback = function (mutations) {
@@ -230,6 +235,7 @@
             if (win.location.href.match(/.*live_chat\?is_popout.*/)) {
                 // chat was opened by user in new window
                 updateStatus(false);
+                viewWindow = null;
             }
             removeEventListener('load', onPopupLoad);
         };
